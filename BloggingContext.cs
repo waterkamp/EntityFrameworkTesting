e69e43232b709334f1702d5058897769b3a13159
Oliver Waterkamp <oliver.waterkamp@2BIT.ch>
2022-06-06 01:42:06 +0200

    unused comment removed

--- BloggingContext.cs
+++ BloggingContext.cs
@@ -22,8 +22,7 @@ namespace EntityFrameworkTesting
             DbPath = System.IO.Path.Join(path, "blogging.db");
         }
 
-        // The following configures EF to create a Sqlite database file in the
-        // special "local" folder for your platform.
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlite($"Data Source={DbPath}");
